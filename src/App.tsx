0ae0087e2e72d6a2e49b3fe0bbb703c5d91810fa
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-07 13:39:09 -0500

    build: configure tailwindcss and integrate with storybook

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <div className="p-10 bg-gray-100 flex justify-center">
+        <h1 className="text-3xl font-bold text-moschino-500 underline">
+          Moschino UI está vivo 🚀
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;

fce9f91bf14b723cae4e86162461c4fe5f3cd9eb
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-08 07:12:44 -0500

    chore: add barrel file (index.ts) to export components publicly

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Exportamos los componentes
+export * from "./components/Button/Button";
+export * from "./components/Input/Input";
+export * from "./components/SearchBar/SearchBar";
+
+// Exportamos las utilidades (opcional, pero muy útil)
+// Esto permite que quien use tu librería pueda usar tu función 'cn' para mezclar clases
+export { cn } from "./lib/utils";
